bd0e11bad6568032523eacf2ebc61234e1cc960b
soobee <20subi@jbnu.ac.kr>
2025-03-02 11:05:22 +0900

    JS: 비밀번호 정규식, 비밀번호 확인, 전화번호 정규식 추가
    ~ JAVASCRIPT 프로젝트 1-3

--- script.js
+++ script.js
@@ -32,10 +32,9 @@ const updateHelperText = (input, message, isValid)=>{
 
 // 입력 필드가 비어있는지 확인하는 함수
 const checkEmptyInput = (input) => {
+    //trim: 인풋 입력칸에 입력한 문자열 중 띄어쓰기를 없애는 기능
     if(input.value.trim() === ''){
-        //인풋 입력칸에 입력한 문자열 중 띄어쓰기를 없애는 기능
         updateHelperText(input,'값을 입력해주세요.',false);
-
         return false;
     }
     else {
@@ -53,6 +52,44 @@ const validateEmailFormat = (input) =>{
         return true;
     } else {
         updateHelperText(input, "유효한 이메일 주소를 입력하세요.", false);
+        return false;
+    }
+}
+
+// 비밀번호 강도 설정
+const checkPasswordStrength = (password)=>{
+    const strongPattern = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+    
+    // 정규식을 만족할 때
+    if(strongPattern.test(password)){
+        updateHelperText(password, "비밀번호 강도: 강함", true);
+        return true;
+    } else {
+        updateHelperText(password, "비밀번호는 8자 이상이어야하며, 대문자, 소문자, 특수문자를 포함하여야 합니다.", false);
+        return false;
+    }
+}
+
+// 비밀번호 확인(동일한지)
+const validatePasswordMatch = (passwordInput, confirmInput)=>{
+    if(passwordInput.value !== confirmInput.value){
+        updateHelperText(confirmInput, "비밀번호가 일치하지 않습니다.", false);
+        return false;
+    } else {
+        updateHelperText(confirmInput, "",true);
+        return true;
+    }
+}
+
+// 전화번호가 올바른 형식인지 확인하는 함수
+const validatePhoneNumber = (input) => {
+    const phonePattern = /^(01[016789]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+
+    if(phonePattern.test(input.value.trim())){
+        updateHelperText(input, "", false);
+    } else {
+        updateHelperText(input, "유효한 전화번호를 입력해주세요.(예: 010-1234-1234)", false);
+        return false;
     }
 }
 
