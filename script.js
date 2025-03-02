406ed2166ae40f70bfe05f6cec2ba8f876015ad2
soobee <20subi@jbnu.ac.kr>
2025-03-02 17:55:59 +0900

    JS: 유효성 검사 완료, 프로젝트 1 완료
    ~  프로젝트1-4

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
+// html 태그와 연결
 const wrapperBox = document.getElementById("wrapper");
-// console.log(wrapperBox); html과 연결 확인
 const inputFieldGroup = document.getElementsByClassName("inputGroup");
 const allInputs = document.querySelector("input");
 const userNickname = document.getElementById("nickname");
 const userEmail = document.getElementById("email");
-const userPassword = document.getElementById("userPassword");
+const userPassword = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 const userPhone = document.getElementById("phone");
 const registrationForm = document.getElementById("registrationForm");
 
 const updateHelperText = (input, message, isValid)=>{
-    const inputGroup = input.parentElement;
+    const inputGroup = input.parentElement; // input 태그의 부모 태그에 접근
 
-    // input 태그의 부모 태그에 접근
+    // heperText(알림)
     const helperText = inputGroup.getElementsByClassName("helperText")[0];
     
     // 유효한 경우 알림 안뜨게
-    if(isValid == true){
+    if(isValid){
         inputGroup.classList.remove("invalid");
         inputGroup.classList.add("valid");
         helperText.style.visibility = "hidden";
-    }
-    // 유효하지 않은 경우 알림보이게
-    if(isValid == false){
+    }else {
         inputGroup.classList.remove("valid");
         inputGroup.classList.add("invalid");
         helperText.style.visibility = "visible";
@@ -30,9 +28,10 @@ const updateHelperText = (input, message, isValid)=>{
     }
 };
 
-// 입력 필드가 비어있는지 확인하는 함수
+// 닉네임 유효성 검사(비어있는지 확인)
 const checkEmptyInput = (input) => {
     //trim: 인풋 입력칸에 입력한 문자열 중 띄어쓰기를 없애는 기능
+    // === 3개는 값의 타입까지 엄격하게 비교
     if(input.value.trim() === ''){
         updateHelperText(input,'값을 입력해주세요.',false);
         return false;
@@ -40,14 +39,15 @@ const checkEmptyInput = (input) => {
     else {
         //입력이 있으면 도움말을 지움
         updateHelperText(input, "", true);
+        return true;
     }
 }
 
-// 이메일 형식이 올바른지 확인하는 함수
-// 이메일 주소가 규칙에 맞게 작성되었는지 확인
+// 이메일 유효성 검사(형식이 올바른지)
 const validateEmailFormat = (input) =>{
+    // 이메일 정규식
     const emailPattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-    if(emailPattern.test(input.value.trim()) == true){
+    if(emailPattern.test(input.value)){
         updateHelperText(input, "", true);
         return true;
     } else {
@@ -56,16 +56,16 @@ const validateEmailFormat = (input) =>{
     }
 }
 
-// 비밀번호 강도 설정
-const checkPasswordStrength = (password)=>{
-    const strongPattern = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/;
+// 비밀번호 유효성 검사
+const checkPasswordStrength = (input)=>{
+    const strongPattern = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
     
-    // 정규식을 만족할 때
-    if(strongPattern.test(password)){
-        updateHelperText(password, "비밀번호 강도: 강함", true);
+    // 그냥 input값을 넘겨줄 경우 html형식. .value 해줘야함
+    if(strongPattern.test(input.value)){
+        updateHelperText(input, "", true);
         return true;
     } else {
-        updateHelperText(password, "비밀번호는 8자 이상이어야하며, 대문자, 소문자, 특수문자를 포함하여야 합니다.", false);
+        updateHelperText(input, "비밀번호는 8자 이상이어야하며, 대문자, 소문자, 특수문자를 포함하여야 합니다.", false);
         return false;
     }
 }
@@ -81,15 +81,64 @@ const validatePasswordMatch = (passwordInput, confirmInput)=>{
     }
 }
 
-// 전화번호가 올바른 형식인지 확인하는 함수
+// 전화번호 유효성 검사
 const validatePhoneNumber = (input) => {
     const phonePattern = /^(01[016789]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
 
     if(phonePattern.test(input.value.trim())){
-        updateHelperText(input, "", false);
+        updateHelperText(input, "", true);
+        return true;
     } else {
         updateHelperText(input, "유효한 전화번호를 입력해주세요.(예: 010-1234-1234)", false);
         return false;
     }
 }
 
+// 전체 유효성 체크 함수
+const validForm = () => {
+    const isNicknameValid = checkEmptyInput(userNickname); // 맞으면 true, 틀리면 false
+    const isEmailValid = validateEmailFormat(userEmail);
+    const isPasswordStrong = checkPasswordStrength(userPassword);
+    const isPasswordMatch = validatePasswordMatch(userPassword, confirmPassword); // 두 비밀번호를 비교
+    const isPhoneValid = validatePhoneNumber(userPhone);
+
+    // 모든 검사에서 통과하는지 검사
+    return isNicknameValid && isEmailValid && isPasswordStrong && isPasswordMatch && isPhoneValid;
+}
+
+// submit이라는 이벤트가 들어왔을 때 유효한지 콘솔에서 확인
+registrationForm.addEventListener('submit',(event)=>{
+    // event: submit하면 실행되는 기능
+    // submit누르면 기본 동작: 새로고침 -> 이걸 막아주는 것이 preventDefault
+    event.preventDefault();
+
+    if(validForm()){
+        console.log("모든 필드가 유효합니다.");
+    } else {
+        console.log("유효성 검사 실패");
+    }
+    console.log(event);
+})
+
+// 각 input을 입력할 때 유효성에 따라 동시에 테두리 색깔이나 일림이 뜨게 설정
+document.querySelectorAll("input").forEach(input =>{
+    input.addEventListener("input",() =>{
+        switch(input.id){
+            case 'nickname':
+                checkEmptyInput(input);
+                break;
+            case 'email':
+                validateEmailFormat(input);
+                break;
+            case 'password':
+                checkPasswordStrength(userPassword);
+                break;
+            case 'confirmPassword':
+                validatePasswordMatch(userPassword, confirmPassword);
+                break;
+            case 'phone':
+                validatePhoneNumber(input);
+                break;
+        }
+    })
+})
