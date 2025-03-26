8a69abfb73daa8f6dc03d0fe97d278bb79726a62
soobee <20subi@jbnu.ac.kr>
2025-03-26 21:23:46 +0900

    SpringBoot: Service 생성

---
+++ SpringBoot/spring_project1/src/main/java/com/canesblack/spring_project1/service/UserService.java
@@ -0,0 +1,24 @@
+package com.canesblack.spring_project1.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.canesblack.spring_project1.entity.User;
+import com.canesblack.spring_project1.mapper.UserMapper;
+
+@Service
+public class UserService {
+	// 의존성 주입
+	@Autowired
+	private UserMapper userMapper;
+	
+	public void insertUser(User user) {
+		userMapper.insertUser(user);
+	}
+	
+	public String findWriter(String username) {
+		return userMapper.findWriter(username);
+	}
+	
+	
+}
