204837516392745d31a18a071127bb3111607155
soobee <20subi@jbnu.ac.kr>
2025-04-04 21:50:36 +0900

    SpringBoot: MenuRestMapper 주석 추가

--- SpringBoot/spring_project1/src/main/java/com/canesblack/spring_project1/mapper/MenuRestMapper.java
+++ SpringBoot/spring_project1/src/main/java/com/canesblack/spring_project1/mapper/MenuRestMapper.java
@@ -11,20 +11,22 @@ import com.canesblack.spring_project1.entity.Menu;
 
 @Mapper
 public interface MenuRestMapper {
+	// 게시글 목록 가져오는 메서드
 	@Select("SELECT idx, memID, title, content, write, indate, count "
 			+ "FROM backend_spring_project.menu ORDER BY idx DESC")
 	public java.util.List<Menu>getLists();
 	
-	
+	// 게시글 추가하는 메서드
 	@Insert("INSERT INTO backend_spring_project.menu(memID, title, content, writer, indate) "
 			+ "VALUES (#{memID}, #{title}, #{content}, #{writer}, #{indate})")
 	public void boarderInsert(Menu menu);
 	
-	
+	// 특정 게시글의 내용을 가져오는 메서드
 	@Select("SELECT idx, memID, title, content, write, indate, count "
 			+ "FROM backend_spring_rpject.menu WHERE idx= #{idx}")
 	public Menu boardContent(int idx);
 	
+	// 특정 게시글을 삭제하는 메서드
 	@Delete("DELETE FROM backend_spring_project.menu WHERE idx=#{idx}")
 	public void boardDelete(int idx);
 	
